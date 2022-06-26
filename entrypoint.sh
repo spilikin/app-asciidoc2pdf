8532ce0f95b57d862664682fdd90badc79906321
spilikin <108782+spilikin@users.noreply.github.com>
2022-06-26 16:55:42 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ asciidoctor-pdf \
     --destination-dir /src/build/ \
     --trace \
     -r asciidoctor-diagram \
-    -a pdf-theme=./theme/gematik-theme.yml \
+    -a pdf-theme=/theme/gematik-theme.yml \
     -a pdf-fontsdir=/theme/fonts/ \
     "$@"
 
